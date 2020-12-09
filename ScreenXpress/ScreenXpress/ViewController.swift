2fece258610758739c7750e528be893497eed4f3
Mondale Felix <mondalefelix@gmail.com>
2020-12-08 23:35:08 -0800

    removed storyboards

---
+++ ScreenXpress/ScreenXpress/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenXpress
+//
+//  Created by Mondale on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
